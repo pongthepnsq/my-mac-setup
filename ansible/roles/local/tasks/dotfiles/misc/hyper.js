6002615b936445db6ac3b8fc08de2b4266668110
Nathakorn Chaninthanadecha <nathakorn.tonch@gmail.com>
2019-10-08 16:53:56 +0700

    add rainglow to hyper.js

--- ansible/roles/local/tasks/dotfiles/misc/hyper.js
+++ ansible/roles/local/tasks/dotfiles/misc/hyper.js
@@ -8,6 +8,7 @@ module.exports = {
     // hyperTransparentDynamic: {
     //   alpha: 0.5 // default 50%
     // },
+    rainglow: 'darkside',
     hyperCustomTouchbar: [
       // if you just need a single button then don't add options array
       { label: 'clear', command: 'clear', backgroundColor: '#d13232' },
@@ -99,7 +100,7 @@ module.exports = {
     'hyper-confirm',
     'hyper-savetext',
     'hyper-custom-touchbar',
-    'hyperblue'
+    "rainglow"
   ],
 
   localPlugins: [],
